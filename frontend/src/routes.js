8ce58b9d26c7cfdbe7d9738d20c5e59d2265273d
Shibata18 <douglas.shibata11@gmail.com>
2020-10-12 19:41:05 -0300

    Adicionando rotas da aplicação

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -6,7 +6,11 @@ import LandingPage from './pages/LandingPage';
 import Login from "./pages/Login";
 import CadastroPage from "./pages/Cadastro";
 import Profile from "./pages/Perfil";
-
+import Cursos from "./pages/Cursos";
+import SocialMedia from "./pages/SocialMedia";
+import Experiencia from "./pages/Experiencia";
+import Objetivo from "./pages/Objetivos";
+import Resume from './pages/Resume'
 const PrivateRoute = ({ component: Component, ...rest }) => (
   <Route
     {...rest}
@@ -25,6 +29,11 @@ const Routes = () => (
     <Switch>
       <Route exact path="/" component={LandingPage} />
       <Route path='/cadastrar' component={CadastroPage} />
+      <Route path='/cursos' component={Cursos} />
+      <Route path='/socialmedia' component={SocialMedia} />
+      <Route path='/experiencia' component={Experiencia} />
+      <Route path='/objetivo' component={Objetivo} />
+      <Route path='/resume' component={Resume} />
       <Route path='/login' component={Login} />
       <PrivateRoute path='/perfil' component={Profile}/> 
       <Route path="*" component={ErrorPage} />
