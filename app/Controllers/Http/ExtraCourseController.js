bfedc136e7bdc739e4684590e9ca9bae5a87870c
Douglas Shibata <douglas.shibata11@gmail.com>
2020-11-04 11:13:30 -0300

    Criando o método POST nos controllers da aplicação

---
+++ app/Controllers/Http/ExtraCourseController.js
@@ -0,0 +1,96 @@
+'use strict'
+const ExtraCourse = use('App/Models/ExtraCourse')
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+
+/**
+ * Resourceful controller for interacting with extracourses
+ */
+class ExtraCourseController {
+  /**
+   * Show a list of all extracourses.
+   * GET extracourses
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async index({ request, response, view }) {
+  }
+
+  /**
+   * Render a form to be used for creating a new extracourse.
+   * GET extracourses/create
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async create({ request, response, view }) {
+  }
+
+  /**
+   * Create/save a new extracourse.
+   * POST extracourses
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async store({ request, response }) {
+    const data = request.only(['courseNameExtra', 'courseLocal', 'courseExtraDescription', 'courseExtraPeriod','user_id'])
+    const extraCourse = await ExtraCourse.create(data)
+    return extraCourse
+  }
+
+  /**
+   * Display a single extracourse.
+   * GET extracourses/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async show({ params, request, response, view }) {
+  }
+
+  /**
+   * Render a form to update an existing extracourse.
+   * GET extracourses/:id/edit
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async edit({ params, request, response, view }) {
+  }
+
+  /**
+   * Update extracourse details.
+   * PUT or PATCH extracourses/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async update({ params, request, response }) {
+  }
+
+  /**
+   * Delete a extracourse with id.
+   * DELETE extracourses/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async destroy({ params, request, response }) {
+  }
+}
+
+module.exports = ExtraCourseController
---
+++ app/Controllers/Http/UserController.js
@@ -0,0 +1,97 @@
+'use strict'
+const User = use("App/Models/User");
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+
+/**
+ * Resourceful controller for interacting with users
+ */
+class UserController {
+  /**
+   * Show a list of all users.
+   * GET users
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async index({ request, response, view }) {
+  }
+
+  /**
+   * Render a form to be used for creating a new experience.
+   * GET users/create
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async create({ request, response, view }) {
+  }
+
+  /**
+   * Create/save a new experience.
+   * POST users
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async store({ request, response }) {
+    const data = request.only(['fullname', 'occupation', 'email', 'address', 'mobilephone', 'homephone','birthday'])
+    const user = await User.create(data)
+    return user
+  }
+
+  /**
+   * Display a single experience.
+   * GET users/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async show({ params, request, response, view }) {
+  }
+
+  /**
+   * Render a form to update an existing experience.
+   * GET users/:id/edit
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async edit({ params, request, response, view }) {
+  }
+
+  /**
+   * Update experience details.
+   * PUT or PATCH users/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async update({ params, request, response }) {
+  }
+
+  /**
+   * Delete a experience with id.
+   * DELETE users/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async destroy({ params, request, response }) {
+  }
+}
+
+module.exports = UserController
