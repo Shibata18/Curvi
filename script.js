4d55c0d574bf398eacc8e4fab2e4449a0d3ad892
Douglas Shibata <douglas.shibata11@gmail.com>
2020-11-09 20:46:15 -0300

    Alterando variaveis e exibiçao das informaçoes

--- script.js
+++ script.js
@@ -7,7 +7,8 @@ var doc = new jsPDF()
       </button>`)
         $.ajax({
             type: 'GET',
-            url: `https://curvi-api.herokuapp.com/user/${$('#user_id').val()}`,
+            //url: `https://curvi-api.herokuapp.com/user/${$('#user_id').val()}`,
+            url: `http://localhost:3333/user/${$('#user_id').val()}`,
             success: function (dados) {
                 doc.text(5, 5, 'Currículo Gerado pela Curvi')
                 console.log(dados);
@@ -23,56 +24,33 @@ var doc = new jsPDF()
                             `)
                     //doc.text(dados.fullname)
                     //doc.text(dados.occupation)
-                    for (let courses of dados.degree) {
                         $('#course').html(
                             `
                             <h3>Formação Acadêmica</h3>
-                            <p>${courses.school}</p>
-                            <p>Período${courses.periodCourse}</p> 
-                            <p>Curso: ${courses.courseName}</p> 
-                            <p>${courses.courseDescription}</p>
+                            <p>${dados.courseSchool}</p>
+                            <p>Período${dados.courseStartYear} - ${dados.courseEndYear}</p> 
+                            <p>Curso: ${dados.courseName}</p> 
                             `)
                         //  doc.text(courses.courseName, 10, 50)
-                    }
-                    for (const xp of dados.experience) {
                         $('#xp').html(
                             `
                             <h3>Experiências</h3>
-                            <p>${xp.cargo}</p> 
-                            <p>${xp.companyName}</p>
-                            <p>${xp.description}</p> 
-                            <p>${xp.period}</p>
+                            <p>${dados.companyOccupation}</p>
+                            <p>${dados.companyStart} - ${dados.companyEnd}</p>  
+                            <p>${dados.companyName}</p>                            
+                            <p>${dados.companyDescription}</p>
                             `)
                         //doc.text(xp.cargo, 10, 100)
-
-                    }
-                    for (const extraCourse of dados.extraCourse) {
-                        $('#extraCourse').html(
-                            `
-                            <h3>Cursos Complementares</h3>
-                            <p>${extraCourse.courseLocal}</p> 
-                            <p>${extraCourse.courseNameExtra}</p>
-                            <p>${extraCourse.courseExtraDescription}</p> 
-                            <p>${extraCourse.courseExtraPeriod}</p>
-                            `)
-                        
-                    }
                         $('#goal').html(
                             `
                             <h3>Objetivo</h3>
                             <p>${ dados.goal}</p> 
-                            `) 
-                        // $('#socialMedia').html(
-                        //     `
-                        //     <h3>Redes Sociais</h3>
-                        //     <p>${ dados.socialMedia.telegram}</p> 
-                        //     <p>${ dados.socialMedia.instagram}</p>
-                        //     `) 
+                            `)  
                 }
                 doc.fromHTML($('.response').get(0), 10, 10, { 'width': 180 });
-                doc.save(`${dados.name}.pdf`)
-                //doc.autoPrint();
-                //doc.output("dataurlnewwindow")
+                //doc.save(`${dados.name}.pdf`)
+                doc.autoPrint();
+                doc.output("dataurlnewwindow")
                 $('.replace').html('<button class="btn btn-success" disabled>Concluído</button>')
             },
             error: function (dados) {
