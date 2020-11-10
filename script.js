fbaf197f7d9456ec932ae98fea4e4f80f98247ce
Douglas Shibata <douglas.shibata11@gmail.com>
2020-11-09 23:27:53 -0300

    Adicionando Cursos

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 var doc = new jsPDF()
-    $('#gerarPDF').on('click', function gerarPDF() {
-        $('.replace').html(`
+$('#gerarPDF').on('click', function gerarPDF() {
+    $('.replace').html(`
         <button class="btn btn-primary" type="button" disabled>
         <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
         Carregando...
       </button>`)
-        $.ajax({
-            type: 'GET',
-            url: `https://curvi-api.herokuapp.com/user/${$('#user_id').val()}`,
-            //url: `http://localhost:3333/user/${$('#user_id').val()}`,
-            success: function (dados) {
-                doc.text(5, 5, 'Currículo Gerado pela Curvi')
-                console.log(dados);
-                for (let i in dados) {
-                    $('#profile').html(
-                        `
+    $.ajax({
+        type: 'GET',
+        url: `https://curvi-api.herokuapp.com/user/${$('#user_id').val()}`,
+        //url: `http://localhost:3333/user/${$('#user_id').val()}`,
+        success: function (dados) {
+            doc.text(5, 5, 'Currículo Gerado pela Curvi')
+            console.log(dados);
+            for (let i in dados) {
+                $('#profile').html(
+                    `
                             <h2>Dados Pessoais</h2>
                             <p>${dados.name}</p>
                             <p>${dados.email}</p>
@@ -22,39 +22,46 @@ var doc = new jsPDF()
                             <p>${dados.city} - ${dados.state}</p>
                             <p>Celular: ${dados.cellphone}</p>
                             `)
-                    //doc.text(dados.fullname)
-                    //doc.text(dados.occupation)
-                        $('#course').html(
-                            `
+                //doc.text(dados.fullname)
+                //doc.text(dados.occupation)
+                $('#course').html(
+                    `
                             <h3>Formação Acadêmica</h3>
                             <p>${dados.courseSchool}</p>
                             <p>Período${dados.courseStartYear} - ${dados.courseEndYear}</p> 
                             <p>Curso: ${dados.courseName}</p> 
                             `)
-                        //  doc.text(courses.courseName, 10, 50)
-                        $('#xp').html(
-                            `
+                //  doc.text(courses.courseName, 10, 50)
+                $('#xp').html(
+                    `
                             <h3>Experiências</h3>
                             <p>${dados.companyOccupation}</p>
                             <p>${dados.companyStart} - ${dados.companyEnd}</p>  
                             <p>${dados.companyName}</p>                            
                             <p>${dados.companyDescription}</p>
                             `)
-                        //doc.text(xp.cargo, 10, 100)
-                        $('#goal').html(
-                            `
+                //doc.text(xp.cargo, 10, 100)
+                $('#goal').html(
+                    `
                             <h3>Objetivo</h3>
-                            <p>${ dados.goal}</p> 
-                            `)  
-                }
-                doc.fromHTML($('.response').get(0), 10, 10, { 'width': 180 });
-                doc.save(`${dados.name}.pdf`)
-                //doc.autoPrint();
-                //doc.output("dataurlnewwindow")
-                $('.replace').html('<button class="btn btn-success" disabled>Concluído</button>')
-            },
-            error: function (dados) {
-                console.log(dados);
+                            <p>${dados.goal}</p> 
+                            `)
+                $('#extraCourse').html(
+                    `
+                                <h3>Cursos Complementares</h3>
+                                <p>${dados.courses}</p> 
+                                `)
+
             }
-        })
+            doc.fromHTML($('.response').get(0), 10, 10, { 'width': 180 });
+            doc.save(`${dados.name}.pdf`)
+            //doc.autoPrint();
+            //doc.output("dataurlnewwindow")
+            $('.replace').html('<button class="btn btn-success" disabled>Concluído</button>')
+        },
+        error: function (dados) {
+            $('.replace').html('<button class="btn btn-warning" disabled>ERRO</button>')
+            console.log(dados);
+        }
     })
+})
